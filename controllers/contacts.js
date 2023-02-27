e0b023a7cc6f3e75efc5f137dfb04ba6d284c4e8
Betelgeuse22 <Don.vitaki@gmail.com>
2023-02-27 22:36:37 +0100

    MongoDB

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,16 +1,15 @@
-const contacts = require("../models/contacts");
+const { Contact } = require("../models/contact");
 
-const { HttpError } = require("../helpers");
-const { ctrlWrapper } = require("../helpers");
+const { ctrlWrapper, HttpError } = require("../helpers");
 
 const listContacts = async (req, res) => {
-  const result = await contacts.listContacts();
+  const result = await Contact.find();
   res.json(result);
 };
 
 const getContactById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contacts.getContactById(contactId);
+  const result = await Contact.findById(contactId);
   if (!result) {
     throw HttpError(404, "Not found");
   }
@@ -18,13 +17,13 @@ const getContactById = async (req, res) => {
 };
 
 const addContact = async (req, res) => {
-  const result = await contacts.addContact(req.body);
+  const result = await Contact.create(req.body);
   res.status(201).json(result);
 };
 
 const removeContact = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contacts.removeContact(contactId);
+  const result = await Contact.findByIdAndRemove(contactId);
   if (!result) {
     throw HttpError(404, "Not found");
   }
@@ -34,13 +33,50 @@ const removeContact = async (req, res) => {
 };
 
 const updateContact = async (req, res) => {
-  const { id } = req.params;
-  const result = await contacts.updateContact(id, req.body);
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
   if (!result) {
     throw HttpError(404, "Not found");
   }
   res.json(result);
 };
+
+const updateFavarite = async (req, res) => {
+  const { contactId } = req.params;
+  if (Object.keys(req.body).length === 0) {
+    throw HttpError(400, "Missing field favorite");
+  }
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
+
+// const updateStatusContact = async (req, res, next) => {
+//   const { contactId } = req.params;
+//   const { error } = favoriteJoiSchema(req.body);
+
+//   if (error) {
+//     res.status(400).json({ message: "missing field favorite" });
+//   }
+//   const { favorite } = req.body;
+//   const result = await Contact.findByIdAndUpdate(
+//     contactId,
+//     { favorite },
+//     {
+//       new: true,
+//     }
+//   );
+//   if (!result) {
+//     return res.status(404).json({ message: "Not found contact" });
+//   }
+//   return res.status(200).json(result);
+// };
 
 module.exports = {
   listContacts: ctrlWrapper(listContacts),
@@ -48,4 +84,5 @@ module.exports = {
   addContact: ctrlWrapper(addContact),
   removeContact: ctrlWrapper(removeContact),
   updateContact: ctrlWrapper(updateContact),
+  updateFavarite: ctrlWrapper(updateFavarite),
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,18 +4,30 @@ const router = express.Router();
 
 const cntrl = require("../../controllers/contacts");
 
-const { validateBody } = require("../../middlewares");
+const { validateBody, isValidId } = require("../../middlewares");
 
-const schemas = require("../../schemas/contacts");
+const { schemas } = require("../../models/contact");
 
 router.get("/", cntrl.listContacts);
 
-router.get("/:contactId", cntrl.getContactById);
+router.get("/:contactId", isValidId, cntrl.getContactById);
 
 router.post("/", validateBody(schemas.postSchema), cntrl.addContact);
 
 router.delete("/:contactId", cntrl.removeContact);
 
-router.put("/:id", validateBody(schemas.putSchema), cntrl.updateContact);
+router.put(
+  "/:contactId",
+  isValidId,
+  validateBody(schemas.putSchema),
+  cntrl.updateContact
+);
+
+router.patch(
+  "/:contactId/favorite",
+  isValidId,
+  validateBody(schemas.updateFavoriteSchema),
+  cntrl.updateFavarite
+);
 
 module.exports = router;
